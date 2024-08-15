368f63b8f1348d4f443723e39c0c366f510e2835
Lucas Kroeger <dev.lucaskroeger@gmail.com>
2024-08-15 19:48:51 -0300

    teste

--- levels/level1.py
+++ levels/level1.py
@@ -32,7 +32,7 @@ class Level1:
         ]
         self.starting_bad_spawn = 5
         self.starting_good_spawn = 5
-        self.bad_spawn_on_good_collection = 1
+        self.bad_spawn_on_good_collection = 3
         self.good_spawn_on_bad_collection = 1
         self.score_to_win = 5
         self.score_to_lose = -3
