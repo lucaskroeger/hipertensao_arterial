58a9c4f7f92a546fc14d2d4ca4afa0d0fe64800b
nicolas <nicolas.souza@moinformatica.com>
2024-09-09 20:10:33 -0300

    mid

--- levels/mid/mid_level.py
+++ levels/mid/mid_level.py
@@ -1,51 +1,43 @@
-class LevelConstructor:
+from levels.level_constructor import LevelConstructor
+
+class MidLevel(LevelConstructor):
+
     def __init__(self):
         self.map = [
             "                                        ",
             "                                        ",
             "                                        ",
+            "    X                             X     ",
             "                                        ",
             "                                        ",
             "                                        ",
+            "    XXX     XXXX               XXXX     ",
             "                                        ",
             "                                        ",
+            "             XXXXXXXXXXXX               ",
             "                                        ",
             "                                        ",
+            "       X               XXX    XXXXX     ",
             "                                        ",
             "                                        ",
+            "      XXX          XXX                XX",
             "                                        ",
             "                                        ",
+            "XX                          XXX    XXXXX",
             "                                        ",
             "                                        ",
+            "        XXXXXXXXXXX                     ",
             "                                        ",
             "                                        ",
-            "                                        ",
-            "                                        ",
-            "                                        ",
-            "                                        ",
-            "                                        ",
-            "                                        ",
-            "                                        ",
-            "                                        ",
+            "                       XXXXXXXX         ",
             "                                        ",
             "                                        "
         ]
         self.starting_bad_spawn = 5
         self.starting_good_spawn = 5
-        self.bad_spawn_on_good_collection = 1
-        self.good_spawn_on_bad_collection = 1
-        self.score_to_win = 5
-        self.score_to_lose = -3
+        self.bad_spawn_on_good_collection = 5
+        self.good_spawn_on_bad_collection = 2
+        self.score_to_win = 6
+        self.maxQuantitySpawn = sum(line.count('X') for line in self.map)
+        self.score_to_lose = -2
         self.background = 'background1'
-
-    def update_player_based_on_score(self, player):
-        if player.last_object == 'G':
-            player.manage_temporary_conditions('slowness', player.default_speed, 0)
-            if player.air_jumps < 1:
-                player.air_jumps += 1
-        if player.last_object == 'B':
-            player.air_jumps = 0
-            player.manage_temporary_conditions('slowness', 5, 5)
-        
-        player.last_object = None
-        return player
